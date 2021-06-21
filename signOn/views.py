a79a1d7a5e3f34a8f67855cda23f9969680c6e84
BrysonWilks <32720810+BrysonWilks@users.noreply.github.com>
2021-06-21 10:33:02 -0500

    update login logic

--- signOn/views.py
+++ signOn/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from . import models
 
 # Create your views here.
@@ -7,15 +8,18 @@ def home(request):
 
 
 def validate(request):
-    u_name = request.POST['username']
-    p_word = request.POST['password']
 
-    #Creating a user for test purposes
-    U1 = models.User()
-    U1.username = "Bob1234"
-    U1.password = "password"
+    if request.method == 'POST':
+        u_name = request.POST['username']
+        p_word = request.POST['password']
 
-    if u_name == U1.username and p_word == U1.password:
-        return render(request, 'userpage.html', {'username' : u_name})
-    else:
-        return render(request, 'home.html', {'error_message': 'Incorrect credentials please try again'})
+        #Creating a user for test purposes
+        U1 = models.User()
+        U1.username = "Bob1234"
+        U1.password = "password"
+
+        if u_name == U1.username and p_word == U1.password:
+            return render(request, 'userpage.html', {'username' : U1.username})
+        else:
+            messages.info(request, "Invalid user credentials")
+            return redirect(home)
