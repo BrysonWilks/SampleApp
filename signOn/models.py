af60440d40966fa0facd837363893164cc42e837
BrysonWilks <32720810+BrysonWilks@users.noreply.github.com>
2021-06-22 14:00:17 -0500

    changing model structure for books

--- signOn/models.py
+++ signOn/models.py
@@ -1,11 +1,5 @@
 from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-    name = models.CharField(max_length=50)
-    age = models.IntegerField()
+from django.contrib.auth.models import User
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
